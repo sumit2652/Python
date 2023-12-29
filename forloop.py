72c81a83d06d392108665de4a215ff98c1cec2ba
sumit2652 <sumit_21@outlook.com>
2023-12-29 23:26:35 +0000

    even number check

---
+++ forloop.py
@@ -0,0 +1,10 @@
+my_iterable  = [1,2,3,4,5,6,7,8,9,10]
+
+for item_name in my_iterable:
+    print(item_name)
+    
+#check for even
+print("Below givens are for even numbers")
+for num in my_iterable:
+    if num % 2 == 0:
+        print(num)

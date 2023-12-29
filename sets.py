d39870e04b5cc716aa9b06ac95c7303e53d9637d
sumit2652 <sumit_21@outlook.com>
2023-12-29 21:18:36 +0000

    list examles in sets

---
+++ sets.py
@@ -0,0 +1,13 @@
+# sets example
+myset = set()
+myset.add(1)
+myset.add(2)
+myset.add(2)
+myset.add('t')
+print(myset)
+
+#what is the use of sets. for exmaple you have a list which have multiple valuse and you want unique values 
+#you can use sets
+
+mylist = [1,1,1,2,2,2,3,3,3,4,4,5,5]
+print(set(mylist))
--- tuples.py
+++ tuples.py
@@ -3,3 +3,5 @@ t = (1,2,3)
 mylist = [1,2,3]
 
 print(type(t))
+
+print(type(mylist))

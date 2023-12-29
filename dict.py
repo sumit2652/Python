08ef16c074377882bfff82324f47e51531eaa630
sumit2652 <sumit_21@outlook.com>
2023-12-29 19:21:22 +0000

    New file added

--- dict.py
+++ dict.py
@@ -1,3 +1,7 @@
 price_lookup = {'apple': 2.99, 'oranges': 1.99, 'milk': 5.87}
+d = {'k1': 123, 'k2':[0,1,2], 'k3': {'insideKey':100}}
 
 print(price_lookup['apple'])
+print(d['k1'])
+print(d['k3'])
+print(d['k3']['insideKey'])

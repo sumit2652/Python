c05d8eebd88669178dbb9766eb4590ce1f58dff2
sumit2652 <sumit_21@outlook.com>
2023-12-29 20:59:28 +0000

    tuples added

--- dict.py
+++ dict.py
@@ -1,7 +1,15 @@
+#Dictionary 
 price_lookup = {'apple': 2.99, 'oranges': 1.99, 'milk': 5.87}
 d = {'k1': 123, 'k2':[0,1,2], 'k3': {'insideKey':100}}
 
-print(price_lookup['apple'])
-print(d['k1'])
-print(d['k3'])
-print(d['k3']['insideKey'])
+# print(d['k1'])
+# print(d['k3'])
+# print(d['k3']['insideKey'])
+
+# add new key value to list
+
+d ['k4'] = 500
+d ['k1'] = 400
+#d.keys()
+d.values()
+print(d)

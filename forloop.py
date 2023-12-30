186fdb655d0499469eeb52b6faaa82be4e68daed
sumit2652 <sumit_21@outlook.com>
2023-12-30 11:24:57 +0000

    for loop added with list

--- forloop.py
+++ forloop.py
@@ -6,3 +6,15 @@ for num in my_iterable:
         print(f'Even number: {num}')
     else:
         print(f'Odd number: {num}')
+list_sum = 0
+
+for num in my_iterable:
+    list_sum = list_sum + num
+    print(list_sum)
+    
+mylist = [(1,2),(3,4),(5,6),(6,7)]
+
+print(len(mylist))
+
+for item in mylist:
+    print(item)
